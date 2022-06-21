15b6a276403d5a40cf120959f50b9abab058b0e4
soxhog <soranatsu.12250511@gmail.com>
2022-06-21 13:58:00 +0900

    add PaymentSplitterUpgradeableCustom

--- contracts/paysplit.sol
+++ contracts/paysplit.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/finance/PaymentSplitterUpgradeable.sol";
+import "./PaymentSplitterUpgradeableCustom.sol";
 import "hardhat/console.sol";
 
 contract PaySplitter is
