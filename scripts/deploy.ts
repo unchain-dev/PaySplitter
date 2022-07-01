d8e42e321aabc235eae57a2aad7b1ce98b81cc49
cohurin <neila@users.noreply.github.com>
2022-07-01 15:40:51 +0900

    update deployment script to use proxy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,29 @@
-import { messagePrefix } from "@ethersproject/hash";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Contract, ContractFactory } from "ethers";
 import { ethers, upgrades } from "hardhat";
 
 const main = async () => {
-    // Deploying
-    const splitter = await ethers.getContractFactory("paysplit");
-    const instance = await upgrades.deployProxy(splitter);
-    await instance.deployed();
+    const spu = ethers.utils.getAddress("0x8dFB9266A18efcDBC1c570248CC47e520D9307B0");
+    const cohurin = ethers.utils.getAddress("0x724C2F25Ca65fef27B2737901FB82D3b86BB6AE3");
+    const misaki = ethers.utils.getAddress("0x6d567CB120d640CE0fd5Ee65893Bd8A70f37D6e3");
+
+    const w_spu:number = 400
+    const w_cohurin:number = 250
+    const w_misaki:number = 250
+
+    const PaySplitterFactory:ContractFactory = await ethers.getContractFactory("PaySplitter");
+    const PaySplitterContract:Contract = await upgrades.deployProxy(
+        PaySplitterFactory, 
+        [[spu, cohurin, misaki], [w_spu, w_cohurin, w_misaki]]
+    );
+    await PaySplitterContract.deployed();
 
     // retrieve contract variables
-    const owner = await instance.signer.getAddress();
+    const owner = await PaySplitterContract.signer.getAddress();
 
-    console.log("Payment splitter contract deployed! Contract address:", instance.address);
+    console.log("Payment splitter contract deployed! Contract address:", PaySplitterContract.address);
     console.log("Contract admin:", owner);
+    console.log("Sho address:", cohurin)
 }
 
 const runMain = async () => {
