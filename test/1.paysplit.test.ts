f1d84a4784f6abcf4267887484e776325f1549c0
SHØ <neila@users.noreply.github.com>
2022-11-21 20:28:51 +0900

    .

--- test/1.paysplit.test.ts
+++ test/1.paysplit.test.ts
@@ -1,23 +1,12 @@
-// We import Chai to use its asserting functions here.
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
 import { BigNumber, Contract, ContractFactory } from "ethers";
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { calculateBalance } from "./utils/calculate";
-// `describe` is a Mocha function that allows you to organize your tests. It's
-// not actually needed, but having your tests organized makes debugging them
-// easier. All Mocha functions are available in the global scope.
-// `describe` receives the name of a section of your test suite, and a callback.
-// The callback must define the tests of that section. This callback can't be
-// an async function.
+
+// Mocha has four functions that let you hook into the the test runner's lifecyle: `before`, `beforeEach`, `after`, `afterEach`.
 
 describe("PaySplitter contract", function () {
-  // Mocha has four functions that let you hook into the the test runner's
-  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
-  // They're very useful to setup the environment for tests, and to clean it
-  // up after they run.
-  // A common pattern is to declare some variables, and assign them in the
-  // `before` and `beforeEach` callbacks.
   let PaySplitterFactory: ContractFactory;
   let contract: Contract;
   let owner: SignerWithAddress;
