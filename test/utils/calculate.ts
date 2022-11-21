a2ee7d27424cf8a7a7b3d111087e3f4b3584ee02
SHØ <neila@users.noreply.github.com>
2022-11-21 20:16:58 +0900

    added test and deployment scripts

--- test/utils/calculate.ts
+++ test/utils/calculate.ts
@@ -1,9 +1,13 @@
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
-export const calculateBalance = async (etherString: string, totalWeight: number, weight: number) => {
-	let etherNumber: number = Number(etherString);
-	etherNumber = etherNumber * weight / totalWeight;
-	let wei: BigNumber = ethers.utils.parseEther(String(etherNumber))
-	return wei;
+export const calculateBalance = async (
+  etherString: string,
+  totalWeight: number,
+  weight: number
+) => {
+  let etherNumber = Number(etherString);
+  etherNumber = (etherNumber * weight) / totalWeight;
+  let wei: BigNumber = ethers.utils.parseEther(String(etherNumber));
+  return wei;
 };
