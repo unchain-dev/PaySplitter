6cf9eced8947537e235b5876d82b5e1c868c0543
SHØ <neila@users.noreply.github.com>
2022-12-14 11:13:45 +0900

    add deploy scripts

---
+++ scripts/tomo.deploy.ts
@@ -0,0 +1,33 @@
+import { Contract, ContractFactory } from "ethers";
+import { ethers, upgrades } from "hardhat";
+
+const main = async () => {
+  const PaySplitterFactory: ContractFactory = await ethers.getContractFactory(
+    "PaySplitter_Tomo",
+  );
+  const PaySplitterContract: Contract = await upgrades.deployProxy(
+    PaySplitterFactory,
+  );
+  await PaySplitterContract.deployed();
+
+  // retrieve contract variables
+  const owner = await PaySplitterContract.signer.getAddress();
+
+  console.log(
+    "Payment splitter contract deployed! Contract address:",
+    PaySplitterContract.address,
+  );
+  console.log("Contract admin:", owner);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
